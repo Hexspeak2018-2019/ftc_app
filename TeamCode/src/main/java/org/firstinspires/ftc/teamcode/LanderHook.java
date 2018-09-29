b18c4ed7595c42bf6dc6fee86720326751c50afc
Sanjana <052014sr@gmail.com>
2018-09-28 22:07:08 -0700

    I wrote the lander hook attachment which includes 2 if statements telling the servo to move forward or backward. I used the d_pad as the buttons for the program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LanderHook.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LanderHook.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
-public class LanderHook {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Drive Teleop", group = "Practice")
+    public class LanderHook extends OpMode{
+
+        Servo landerHook;
+
+        @Override
+        public void init() {
+            landerHook =hardwareMap.servo.get("hook");
+        }
+
+        @Override
+        public void loop() {
+            landerHook.setDirection(Servo.Direction.FORWARD);
+
+            if(gamepad1.dpad_up = true) {
+                    double positionCurrent = landerHook.getPosition();
+                    double positionNew = positionCurrent + 0.01;
+                    landerHook.setPosition(positionNew);
+            }
+
+            if(gamepad1.dpad_down = true) {
+                    double positionCurrent2 = landerHook.getPosition();
+                    double positionNew2= positionCurrent2 - 0.01;
+                    landerHook.setPosition(positionNew2);
+            }
+
+        }
+
 }
